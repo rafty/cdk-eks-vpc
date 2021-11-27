a85b62bab14706c6dbf4a7650ee7941530c05a1d
yagita <yagita.takashi@gmail.com>
2021-11-27 21:28:36 +0900

    Initial commit

---
+++ tests/unit/test_cdk_eks_vpc_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_eks_vpc.cdk_eks_vpc_stack import CdkEksVpcStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_eks_vpc/cdk_eks_vpc_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkEksVpcStack(app, "cdk-eks-vpc")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
